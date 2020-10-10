edb6e63c2e7a360ae4697576377fb3cbff3c3943
SMore-Napi <s.roma245@gmail.com>
2020-10-10 19:40:43 +0400

    Added Phrasebook list

---
+++ InnoRussian/app/src/main/java/com/example/innorussian/PhraseActivity.kt
@@ -0,0 +1,20 @@
+package com.example.innorussian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_phrase.*
+
+class PhraseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phrase)
+
+        word.text = this.intent.getStringExtra("word")
+        transcription.text = this.intent.getStringExtra("transcription")
+        translation.text = this.intent.getStringExtra("translation")
+
+        back_button.setOnClickListener {
+            finish()
+        }
+    }
+}
--- InnoRussian/app/src/main/java/com/example/innorussian/PhrasebookFragment.kt
+++ InnoRussian/app/src/main/java/com/example/innorussian/PhrasebookFragment.kt
@@ -1,33 +1,81 @@
 package com.example.innorussian
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_phrasebook.*
 
 class PhrasebookFragment : Fragment(R.layout.fragment_phrasebook) {
+    private var phrasesArray: ArrayList<Phrases> = ArrayList()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val phrasesArray: ArrayList<String> = getPhrases()
+        phrasesArray = getPhrasesArray()
+        val previewPhrasesArray: ArrayList<String> = getPhrasesPreviewArray()
 
         val adapter = activity?.let {
             ArrayAdapter<String>(
-                it, android.R.layout.simple_list_item_1, phrasesArray
+                it, android.R.layout.simple_list_item_1, previewPhrasesArray
             )
         }
         phrases.adapter = adapter
+
+        phrases.setOnItemClickListener { parent, view, position, id ->
+
+            for (currentPhrase in previewPhrasesArray) {
+                if ((view as TextView).text == currentPhrase) {
+
+                    val string = currentPhrase.split(" / ")
+                    var phraseToShow = Phrases("", "", "")
+
+                    for (i in phrasesArray) {
+                        if (i.word == string[0]) {
+                            phraseToShow = Phrases(i.word, i.translation, i.transcription)
+                            break
+                        }
+                    }
+
+                    val phraseIntent = Intent(activity, PhraseActivity::class.java)
+                    phraseIntent.putExtra("word", phraseToShow.word)
+                    phraseIntent.putExtra("transcription", phraseToShow.transcription)
+                    phraseIntent.putExtra("translation", phraseToShow.translation)
+                    startActivity(phraseIntent)
+
+                }
+            }
+        }
     }
 
-    private fun getPhrases(): ArrayList<String> {
-        val phrasesArray: ArrayList<String> = ArrayList()
-        phrasesArray.add("Привет Hello")
-        phrasesArray.add("Как дела? How are you?")
-        phrasesArray.add("Как погода? How is the weather?")
-        phrasesArray.add("АЗАЗА")
+    private fun getPhrasesArray(): ArrayList<Phrases> {
 
+        val w1 = Phrases("Привет", "Hello", "auditoriya")
+        val w2 = Phrases("Как дела?", "How are you?", "stolovaya")
+        val w3 = Phrases("Как погода?", "How is the weather?", "zanyatiye")
+        val w4 = Phrases("АЗАЗА", "AZAZA", "AZAZA")
 
-        return phrasesArray
+        return arrayListOf(w1, w2, w3, w4)
     }
+
+    private fun getPhrasesPreviewArray(): ArrayList<String> {
+        val previewPhrasesArray: ArrayList<String> = ArrayList()
+        for (phrase in phrasesArray) {
+            previewPhrasesArray.add(phrase.word.plus(" / ").plus(phrase.translation))
+
+        }
+
+        return previewPhrasesArray
+    }
+
+    class Phrases(
+        val word: String,
+        val translation: String,
+        val transcription: String
+    )
 }
